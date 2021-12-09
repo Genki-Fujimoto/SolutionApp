234e62392f61dd7c9d134cbf50b8a35767645a8c
Genki <fg2920@yoshida-dental.co.jp>
2021-12-09 22:06:39 +0900

    Initial Commit

---
+++ SolutionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SolutionApp
+//
+//  Created by GENKI Mac on 2021/12/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
